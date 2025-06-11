b8adf5c4fcfa9c8142243c32f7f8598a2e555413
kushal9897 <kushalag2580@gmail.com>
2025-06-12 01:16:56 +0530

    adding org1

---
+++ namespaces/org1/main.tf
@@ -0,0 +1,7 @@
+module "team_aa" {
+  source        = "./modules"
+  ami           = var.ami
+  instance_type = "t2.micro"
+  name          = "team-a-instance"
+  team          = "team-a"
+}
---
+++ namespaces/org1/output.tf
@@ -0,0 +1,3 @@
+output "team_a_instance_id" {
+  value = module.team_a.instance_id
+}
