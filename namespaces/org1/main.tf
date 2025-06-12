0d992e2be5eac37d423f257ae5aeb8dcfa42f09b
kushal9897 <kushalag2580@gmail.com>
2025-06-13 01:34:45 +0530

    testing

--- namespaces/org1/main.tf
+++ namespaces/org1/main.tf
@@ -1,4 +1,4 @@
-module "team_aa" {
+module "team_a" {
   source        = "./modules"
   ami           = var.ami
   instance_type = "t2.micro"
--- namespaces/org2/main.tf
+++ namespaces/org2/main.tf
@@ -1,7 +1,7 @@
-module "team_aa" {
+module "team_b" {
   source        = "./modules"
   ami           = var.ami
   instance_type = "t2.micro"
-  name          = "team-a-instance"
+  name          = "team-b-instance"
   team          = "team-b"
 }
